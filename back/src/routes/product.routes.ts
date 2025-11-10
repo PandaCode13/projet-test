5f392a65bf1bedfa0a95a9dab5ae62627a3205b8
0xOthmane <0xothodor@proton.me>
2025-11-10 13:59:25 +0100

    feat: add create product route

---
+++ back/src/routes/product.routes.ts
@@ -0,0 +1,8 @@
+import { getProducts } from '#controllers/product.controller.js';
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/products', getProducts);
+
+export default router;
--- back/src/types/products.validation.ts
+++ back/src/types/products.validation.ts
@@ -1,9 +1,21 @@
 import z from 'zod';
 
-export const productsSchema = z.object({
+export const productsQuerySchema = z.object({
   query: z.object({
     query: z.string({ error: 'Search query is required' }),
     page: z.string().optional(),
     external: z.enum(['true', 'false']).optional()
+  })
+});
+
+export const createProductSchema = z.object({
+  body: z.object({
+    barcode: z.string({ error: 'Product code is required' }),
+    name: z.string({ error: 'Product name is required' }),
+    brand: z.string().optional(),
+    imageUrl: z.url().optional(),
+    sugar: z.number().optional(),
+    calories: z.number().optional(),
+    caffeine: z.number().optional()
   })
 });
